0adef3c2cf13e40dbf22a1edf63658a129df8df3
npawar26 <nikhilpwar123@gmail.com>
2022-04-25 19:17:35 +0530

    Adding-VowelConsonant Programm

---
+++ VowelConsonant.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.day5_assignment;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) 
+	{	
+		char ch = 'r';
+
+		if(ch == 'a' || ch == 'e'  || ch == 'i' || ch == 'o' || ch == 'u')
+		{
+			System.out.println("The given variable is Vowel");
+		}
+		else
+		{
+			System.out.println("The given variable is consonant");
+		}
+	}
+
+}
